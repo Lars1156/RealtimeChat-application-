111fde4a0a4f3aaf8ec3898d5b753bd65ac8ca25
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-10-22 12:08:00 +0530

    the done with userController part

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,1 +1,37 @@
 const userServices = require('../services/userServices');
+// User registration Controller 
+const register = async(req,res)=>{
+    try {
+        const { username, email, password } = req.body;
+        const token = await userServices.registerUser(username, email, password);
+        res.json({ token });
+    } catch (error) {
+        return res.status(400).json('Internal Server Error');
+    }
+};
+// Login User Controller
+const login = async(req,res) =>{
+    try {
+          const {email , password} = req.body;
+          const toekn = await userServices.loginUser(email , password);
+          res.json({toekn});
+    } catch (error) {
+        
+    }
+}
+// / Get user info
+const getUser = async (req, res) => {
+  try {
+    const user = await userServices.getUserById(req.user.id);
+    res.json(user);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+
+module.exports = {
+    register,
+    login,
+    getUser
+}
