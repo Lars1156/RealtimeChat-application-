2c603ae9acc8fdd9a4931fb6bed1f7ecee25feea
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-10-22 13:37:54 +0530

    create the router Api of all controllerss

--- backend/Router/api.js
+++ backend/Router/api.js
@@ -2,7 +2,15 @@ const express = require ('express');
 const router = express.Router();
 const userController = require('../controller/userController');
 const messageController = require('../controller/messageController');
+const conversationController = require('../controller/conversationController');
+const authMiddleware = require('../middleware/auth');
 
 router.post('/register' , userController.register);
 router.post ('./login', userController.login);
-router.get('./getUser' , userController.getUser);
+router.get('./getUser' , authMiddleware, userController.getUser);
+
+router.post ('/sendMessage' , authMiddleware ,messageController.createMessage);
+router.get('/getMessage/:conversationId',authMiddleware,messageController.getMessages);
+
+router.post('/crateConversation' , authMiddleware , conversationController.createConversation);
+router.get('/getConversation' , authMiddleware , conversationController.getUserConversations);
