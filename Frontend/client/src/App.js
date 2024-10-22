42f09a4061e1d4361ca7815f08e3796b8a904dec
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-10-22 15:24:40 +0530

    add the changes of the register user

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -1,10 +1,15 @@
-import {Router , Routes} from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
 import CustomNavbar from './components/Navbar';
+import Register from './components/Register';
 
 function App() {
   return (
     <div className="App">
       <CustomNavbar/>
+      <Routes>
+        
+           <Route path='/profile' element={<Register/>}/>
+      </Routes>
     </div>
   );
 }
--- Frontend/client/src/components/Register.js
+++ Frontend/client/src/components/Register.js
@@ -1,11 +1,64 @@
 import React from "react";
+import { UserOutlined, MailOutlined, LockOutlined } from '@ant-design/icons';
+import { Form, Button } from 'react-bootstrap';
 
 const Register = () =>{
     return(
         <>
-          <div>
-            This is Register pages
+          <div className="container mt-5">
+      <h2 className="text-center">Register_User</h2>
+       <p className="text-danger"></p>
+      <Form  className="mx-auto" style={{ maxWidth: '400px' }}>
+        <Form.Group>
+          <Form.Label>Username</Form.Label>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text"><UserOutlined /></span>
+            </div>
+            <Form.Control
+              type="text"
+              name="username"
+              
+              placeholder="Username"
+              required
+            />
           </div>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Email</Form.Label>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text"><MailOutlined /></span>
+            </div>
+            <Form.Control
+              type="email"
+              name="email"
+             
+              placeholder="Email"
+              required
+            />
+          </div>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text"><LockOutlined /></span>
+            </div>
+            <Form.Control
+              type="password"
+              name="password"
+             
+              placeholder="Password"
+              required
+            />
+          </div>
+        </Form.Group>
+        <Button variant="primary" type="submit" className="w-100">
+          Register
+        </Button>
+      </Form>
+    </div>
         </>
     )
 }
